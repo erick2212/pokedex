88d2acc3a31c0df87ca77b189cb09a9f872f331c
erick <erick221294@gmail.com>
2021-04-09 19:52:51 -0500

    prueba 1, pokedex

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function sub()
+{
+    
+    const url = 'https://pokeapi.co/api/v2/pokemon/'
+    var nombre = document.getElementsByName("pok")[0].value;
+    var pokemon = url+nombre
+
+    fetch(pokemon)
+    .then(response => response.json())
+    .then(data =>
+        {
+            let element = document.getElementById('elem')
+            element.innerHTML = `
+            <p>Nombre: ${data.name}</p>
+            <p>Altura: ${data.height}</p>
+            <p>Peso: ${data.weight} cm</p>
+            `;
+            console.log(data)
+        })
+    .catch(err => console.log(err))
+    if(!response){
+        alert("no se encontro tu pokemon")
+    };
+};
